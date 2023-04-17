55d3e63192ec333eb774375c57bf8d6b8ecc5ccb
pixmin94 <pixmin@outlook.com>
2023-04-17 15:08:35 +0800

    update codes til slide 19

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -23,18 +23,53 @@ public class App
             System.err.println("You have not told me your name");
         };
 
+        // slide 17 
         String input = con.readLine("\nWhat is your hobby?");
         input = input.trim();
 
-        if (input.equals("swim")) {
-            System.out.println("The nearest swimming pool is Clementi");
-        } else if (input.equals("jog")) {
-            System.out.println("The nearest park is West Coast Park");
-        } else if (input.equals("cycle")) {
-            System.out.println("You could cycle along the PCN");
+        // if (input.equals("swim")) {
+        //     System.out.println("The nearest swimming pool is Clementi");
+        // } else if (input.equals("jog")) {
+        //     System.out.println("The nearest park is West Coast Park");
+        // } else if (input.equals("cycle")) {
+        //     System.out.println("You could cycle along the PCN");
+        // } else {
+        //     System.out.println("Seems like you are a boring person without hobbies");
+        // }
+
+        //slide 18
+        switch(input) {
+            case "swim":
+                System.out.println("The nearest swimming pool is Clementi");
+                break;
+            case "jog":
+                System.out.println("The nearest park is West Coast Park");
+                break;
+            case "cycle":
+                System.out.println("You could cycle along the PCN");
+                break;
+            default:
+                System.out.println("Seems like you are a boring person without hobbies");
+                break;
+        }   
+
+        //slide 19
+        Integer myAge = 0;
+        String myAgeInput = con.readLine("What is your age? ");
+        myAge = Integer.parseInt(myAgeInput);
+
+        if (myAge <= 0)   {
+            System.err.println("Are you sure?");
+        } else if ((myAge > 0) && (myAge < 7)) {
+            System.out.println("You are a toddler");
+        } else if ((myAge >= 7) && (myAge < 12)) {
+            System.out.println("You are a child");
+        } else if ((myAge >= 12) && (myAge < 18)) {
+            System.out.println("You are a teen");
         } else {
-            System.out.println("Seems like you are a boring person without hobbies");
+            System.out.println("You are an adult");
         }
+    
 
     }
 }
